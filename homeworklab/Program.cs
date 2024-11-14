34b84b48a218043b1f40f2a938b9600ea9f154ef
sanyochek1 <tyryskinsasa4@gmail.com>
2024-11-14 23:27:50 +0300

    лаб.работа

---
+++ homeworklab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+enum AccountType
+{
+    Текущий,
+    Сберегательный
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("№1lab");
+        /*Создать перечислимый тип данных отображающий виды банковского
+счета(текущий и сберегательный).Создать переменную типа перечисления, присвоить ей
+значение и вывести это значение на печать.*/
+
+        AccountType myAccount = AccountType.Сберегательный;
+        Console.WriteLine("Тип банковского счета: " + myAccount);
+    }
+}
